f6d4a71641702c3b9bf6a66f4951a04075dc5a30
Joyce Miryam <jhabb071@uottawa.ca>
2016-06-21 09:44:49 -0400

    Create pairSum.py

---
+++ python/pairSum.py
@@ -0,0 +1,13 @@
+def pairSum(arr, k):
+    if len(arr) < 2:
+        return
+    arr.sort()
+    left, right = (0, len(arr)-1)
+    while left<right:
+        tempSum=arr[left]+arr[right]
+        if tempSum=k:
+            print arr[left], arr[right]
+        elif tempSum > k:
+            right-=1
+        else
+            left+=1
