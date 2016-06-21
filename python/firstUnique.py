ca145584f52370c655d87493eda29e85fdfdbc3c
Joyce Miryam <jhabb071@uottawa.ca>
2016-06-21 10:29:07 -0400

    Create firstUnique.py

---
+++ python/firstUnique.py
@@ -0,0 +1,10 @@
+# Find the first non-repeated (unique) character in a given string.
+# efficient use of a hashtable needed.
+
+def firstUnique(str):
+    count=collections.defaultdict(int)
+    for letter in str:
+        count[letter]+=1
+    for letter in str
+        if count[letter]==1
+        return letter
